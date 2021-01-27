d3bd6e2b2f8fd3e97ca0810bf35b277ea56f78a0
Allyson Muriel <amuriel@am-j4.msk.21-school.ru>
2021-01-27 16:59:34 +0300

    3day

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: amuriel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 16:57:45 by amuriel           #+#    #+#             */
-/*   Updated: 2021/01/27 14:21:20 by amuriel          ###   ########.fr       */
+/*   Updated: 2021/01/27 16:07:58 by amuriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 # include <stdio.h> //убрать потом
 # include "libft/libft.h"
 # include "srcs/gnl/get_next_line.h"
+
+# include "minilibx_opengl/mlx.h"
+
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
--- srcs/main.c
+++ srcs/main.c
@@ -6,21 +6,73 @@
 /*   By: amuriel <amuriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 17:08:51 by amuriel           #+#    #+#             */
-/*   Updated: 2021/01/27 14:49:53 by amuriel          ###   ########.fr       */
+/*   Updated: 2021/01/27 16:12:58 by amuriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-int main(void)
-{
-	char	*line;
+//int main(void)
+//{
+//	char	*line;
+//
+//	int		fd = open("./maps/map.cub", O_RDONLY);
+//	while (get_next_line(fd, &line) > 0)
+//	{
+//		printf("%s\n", line);
+//		free(line);
+//	}
+//	free(line);
+//
+//}
 
-	int		fd = open("./maps/map.cub", O_RDONLY);
-	while (get_next_line(fd, &line) > 0)
+# define WIN_WIDTH 800
+# define WIN_HEIGHT 600
+
+# define IMG_WIDTH 400
+# define IMG_HEIGHT 300
+
+typedef struct	s_img
+{
+	void		*img_ptr;
+	int			*data;
+	int			size_l;
+	int			bpp;
+	int			endian;
+}				t_img;
+
+typedef struct	s_mlx
+{
+	void		*mlx_ptr;
+	void		*win;
+}				t_mlx;
+
+int	main(void)
+{
+	t_mlx	*mlx;
+	int		count_w;
+	int		count_h;
+	t_img	img;
+
+	mlx->mlx_ptr = mlx_init();
+	mlx->win = mlx_new_window(mlx->mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "A simple example");
+	img.img_ptr = mlx_new_image(mlx->mlx_ptr, IMG_WIDTH, IMG_HEIGHT);
+	img.data = (int *)mlx_get_data_addr(img.img_ptr, &img.bpp, &img.size_l, &img.endian);
+
+
+	count_h = -1;
+	while (++count_h < IMG_HEIGHT)
 	{
-		printf("%s\n", line);
-		free(line);
+		count_w = -1;
+		while (++count_w < IMG_WIDTH)
+		{
+			if (count_w % 2)
+				img.data[count_h * IMG_WIDTH + count_w] = 0xFFFFFF;
+			else
+				img.data[count_h * IMG_WIDTH + count_w] = 0xFF0000;
+		}
 	}
-	free(line);
+	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win, img.img_ptr, 0, 0);
+	mlx_loop(mlx->mlx_ptr);
+	return (0);
 }
